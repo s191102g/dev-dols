6ea1113ad2b44023a05c18628654c7675d2ba44e
sang2xx2 <s191102g@gmail.com>
2022-12-18 13:33:06 +0700

    a

--- src/infras/api/AppService.ts
+++ src/infras/api/AppService.ts
@@ -1,68 +1,67 @@
-import "reflect-metadata";
-import express from "express";
-import path from "path";
-import { RoutingControllersOptions } from "routing-controllers";
-import { ApiDocument } from "./ApiDocument";
-import swaggerUiExpress from "swagger-ui-express";
-import Container from "typedi";
-import { ILogService } from "../../core/gateways/services/ILogService";
-import { HttpServer } from "../servers/HttpServer";
-import { Server } from "http";
-import { ApiAuthenticator } from "./ApiAuthenticator";
+// import "reflect-metadata";
+// import express from "express";
+// import path from "path";
+// import { RoutingControllersOptions } from "routing-controllers";
+// import { ApiDocument } from "./ApiDocument";
+// import swaggerUiExpress from "swagger-ui-express";
+// import Container from "typedi";
+// import { ILogService } from "../../core/gateways/services/ILogService";
+// import { HttpServer } from "../servers/HttpServer";
+// import { Server } from "http";
+// import { ApiAuthenticator } from "./ApiAuthenticator";
+// export class ApiService {
+//   static init(port: number, callback?: () => void): Server {
+//     const app = express();
+//     const logger = Container.get<ILogService>("log.service");
+//     app.get("/", (_req, res) => {
+//       res.status(200).end("ok");
+//     });
+//     app.get("/.well-known/pki-validation/1F7DE192245FF311D2556D6C890BB999.txt", (_req, res) => {
+//       res.sendFile('/home/ec2-user/dev-dols/1F7DE192245FF311D2556D6C890BB999.txt')
+//     });
 
-export class ApiService {
-  static init(port: number, callback?: () => void): Server {
-    const app = express();
-    const logger = Container.get<ILogService>("log.service");
-    app.get("/", (_req, res) => {
-      res.status(200).end("ok");
-    });
-    app.get("/.well-known/pki-validation/1F7DE192245FF311D2556D6C890BB999.txt", (_req, res) => {
-      res.sendFile('/home/ec2-user/dev-dols/1F7DE192245FF311D2556D6C890BB999.txt')
-    });
+//     const loggingMiddleware = logger.createMiddleware();
+//     app.use(loggingMiddleware);
+//     const options = this.getOptions({
+//       controllers: [path.join(__dirname + "/controllers/**/*{.js,.ts}")],
+//       middlewares: [path.join(__dirname, "./middlewares/*Middleware{.js,.ts}")],
+//       validation: false,
+//       development: !!(process.env.NODE_ENV != "production"),
+//     });
+//     const httpServer = new HttpServer();
+//     httpServer.createApp(app, options);
+//     const spec = ApiDocument.generate(options);
+//     app.use("/docs", swaggerUiExpress.serve, swaggerUiExpress.setup(spec));
+//     return httpServer.start(port, callback);
+//   }
 
-    const loggingMiddleware = logger.createMiddleware();
-    app.use(loggingMiddleware);
-    const options = this.getOptions({
-      controllers: [path.join(__dirname + "/controllers/**/*{.js,.ts}")],
-      middlewares: [path.join(__dirname, "./middlewares/*Middleware{.js,.ts}")],
-      validation: false,
-      development: !!(process.env.NODE_ENV != "production"),
-    });
-    const httpServer = new HttpServer();
-    httpServer.createApp(app, options);
-    const spec = ApiDocument.generate(options);
-    app.use("/docs", swaggerUiExpress.serve, swaggerUiExpress.setup(spec));
-    return httpServer.start(port, callback);
-  }
-
-  static getOptions(param: {
-    controllers?: string[] | Function[];
-    middlewares?: string[] | Function[];
-    validation: boolean;
-    development: boolean;
-  }): RoutingControllersOptions {
-    return {
-      cors: {
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowedHeaders: [
-          "Origin",
-          "Content-Type",
-          "Accept",
-          "Authorization",
-          "X-Trace",
-        ],
-        preflightContinue: true,
-        optionsSuccessStatus: 204,
-      },
-      routePrefix: "/api",
-      controllers: param.controllers,
-      middlewares: param.middlewares,
-      validation: param.validation,
-      development: param.development,
-      authorizationChecker: ApiAuthenticator.authorizationChecker,
-      currentUserChecker: ApiAuthenticator.currentUserChecker,
-    };
-  }
-}
+//   static getOptions(param: {
+//     controllers?: string[] | Function[];
+//     middlewares?: string[] | Function[];
+//     validation: boolean;
+//     development: boolean;
+//   }): RoutingControllersOptions {
+//     return {
+//       cors: {
+//         origin: "*",
+//         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+//         allowedHeaders: [
+//           "Origin",
+//           "Content-Type",
+//           "Accept",
+//           "Authorization",
+//           "X-Trace",
+//         ],
+//         preflightContinue: true,
+//         optionsSuccessStatus: 204,
+//       },
+//       routePrefix: "/api",
+//       controllers: param.controllers,
+//       middlewares: param.middlewares,
+//       validation: param.validation,
+//       development: param.development,
+//       authorizationChecker: ApiAuthenticator.authorizationChecker,
+//       currentUserChecker: ApiAuthenticator.currentUserChecker,
+//     };
+//   }
+// }
---
+++ src/infras/api/AppService2.ts
@@ -0,0 +1,82 @@
+import "reflect-metadata";
+import express from "express";
+import path from "path";
+import { RoutingControllersOptions } from "routing-controllers";
+import { ApiDocument } from "./ApiDocument";
+import swaggerUiExpress from "swagger-ui-express";
+import Container from "typedi";
+import { ILogService } from "../../core/gateways/services/ILogService";
+import { HttpServer } from "../servers/HttpServer";
+// import { Server } from "http";
+import { ApiAuthenticator } from "./ApiAuthenticator";
+import * as fs from 'fs';
+import * as https from 'https'
+export class ApiService2 {
+  static init(port: number, callback?: () => void): void {
+    const app = express();
+    const logger = Container.get<ILogService>("log.service");
+    app.get("/", (_req, res) => {
+      res.status(200).end("ok");
+    });
+    app.get("/.well-known/pki-validation/1F7DE192245FF311D2556D6C890BB999.txt", (_req, res) => {
+      res.sendFile('/home/ec2-user/dev-dols/1F7DE192245FF311D2556D6C890BB999.txt')
+    });
+    const key = fs.readFileSync('./src/private.key');
+    const cert= fs.readFileSync('./src/certificate.crt')
+    const loggingMiddleware = logger.createMiddleware();
+    app.use(loggingMiddleware);
+    const options = this.getOptions({
+      controllers: [path.join(__dirname + "/controllers/**/*{.js,.ts}")],
+      middlewares: [path.join(__dirname, "./middlewares/*Middleware{.js,.ts}")],
+      validation: false,
+      development: !!(process.env.NODE_ENV != "production"),
+    });
+    const httpServer = new HttpServer();
+    httpServer.createApp(app, options);
+    const spec = ApiDocument.generate(options);
+    app.use("/docs", swaggerUiExpress.serve, swaggerUiExpress.setup(spec));
+     httpServer.start(port, callback);
+     https.createServer({ key, cert })
+     .listen(3002,()=>{
+      console.log('https');
+      
+     })
+  }
+
+  static getOptions(param: {
+    controllers?: string[] | Function[];
+    middlewares?: string[] | Function[];
+    validation: boolean;
+    development: boolean;
+  }): RoutingControllersOptions {
+    return {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: [
+          "Origin",
+          "Content-Type",
+          "Accept",
+          "Authorization",
+          "X-Trace",
+        ],
+        preflightContinue: true,
+        optionsSuccessStatus: 204,
+      },
+      routePrefix: "/api",
+      controllers: param.controllers,
+      middlewares: param.middlewares,
+      validation: param.validation,
+      development: param.development,
+      authorizationChecker: ApiAuthenticator.authorizationChecker,
+      currentUserChecker: ApiAuthenticator.currentUserChecker,
+    };
+  }
+}
+
+
+
+
+
+
+
